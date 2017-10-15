e72fb26216d3834c6009a0e2d96d45afabc5899a
mas9612 <mas9612@gmail.com>
2017-10-15 21:40:35 +0900

    add -f option to ln

--- setup.sh
+++ setup.sh
@@ -2,22 +2,22 @@
 
 PWD=`pwd`
 
-ln -s $PWD/zshenv ~/.zshenv
-ln -s $PWD/zshrc ~/.zshrc
-ln -s $PWD/tmux.conf ~/.tmux.conf
+ln -sf $PWD/zshenv ~/.zshenv
+ln -sf $PWD/zshrc ~/.zshrc
+ln -sf $PWD/tmux.conf ~/.tmux.conf
 
-ln -s $PWD/ctags ~/.ctags
-ln -s $PWD/latexmkrc ~/.latexmkrc
+ln -sf $PWD/ctags ~/.ctags
+ln -sf $PWD/latexmkrc ~/.latexmkrc
 
 if [ ! -d ~/.vim ]; then
     mkdir -f ~/.vim
 fi
-ln -s $PWD/vim/vimrc ~/.vimrc
-ln -s $PWD/vim/gvimrc ~/.gvimrc
-ln -s $PWD/vim/colors ~/.vim/colors
-ln -s $PWD/vim/ftplugin ~/.vim/ftplugin
-ln -s $PWD/vim/template ~/.vim/template
-ln -s $PWD/vim/snippets ~/.vim/snippets
+ln -sf $PWD/vim/vimrc ~/.vimrc
+ln -sf $PWD/vim/gvimrc ~/.gvimrc
+ln -sf $PWD/vim/colors ~/.vim/colors
+ln -sf $PWD/vim/ftplugin ~/.vim/ftplugin
+ln -sf $PWD/vim/template ~/.vim/template
+ln -sf $PWD/vim/snippets ~/.vim/snippets
 
 if [ `which pip` ]; then
     pip install -r $PWD/pip_requirements.txt
