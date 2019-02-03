9d7c59e21ddada9c95871c89e9d577382986cb68
mas9612 <mas9612@gmail.com>
2019-02-03 22:47:18 +0900

    setup.sh: update setup script

--- setup.sh
+++ setup.sh
@@ -2,12 +2,28 @@
 
 cd `dirname $0`
 
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew bundle
-brew cleanup
+if [ "`uname -s`" = "Darwin" ]; then
+    echo "Checking whether Homebrew is installed..."
+    which brew
+    if [ $? = 0 ]; then
+        echo "Homebrew is already installed."
+    else
+        echo "It seems Homebrew is not installed."
+        echo "Installing homebrew..."
+        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    fi
 
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    echo "Installing softwares..."
+    brew bundle
+    brew cleanup
+fi
 
+if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+    echo "Installing tmux plugin manager..."
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
+echo "Linking config files..."
 ln -sf $PWD/.zshenv ~/.zshenv
 ln -sf $PWD/.zshrc ~/.zshrc
 ln -sf $PWD/.tmux.conf ~/.tmux.conf
@@ -19,8 +35,11 @@ ln -sf $PWD/.vimrc ~/.vimrc
 ln -sf $PWD/.gvimrc ~/.gvimrc
 ln -sf $PWD/.vim ~/.vim
 
-# install vim plugins
+echo "Installing Vim plugins..."
 vim -c ':PlugInstall | :qa'
 
-# Run Adobe Creative Cloud installer manually
-open "/usr/local/Caskroom/adobe-creative-cloud/latest/Creative Cloud Installer.app"
+if [ "`uname -s`" = "Darwin" -a ! -d "/Applications/Adobe Creative Cloud" ]; then
+    open "/usr/local/Caskroom/adobe-creative-cloud/latest/Creative Cloud Installer.app"
+fi
+
+echo "Completed!"
