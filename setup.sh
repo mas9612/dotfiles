7720c0a18a093d057b9ef816f995cbda75ba648c
Masato Yamazaki <masato-yamazaki@cybozu.co.jp>
2024-05-16 13:51:57 +0900

    add vscode settings

--- setup.sh
+++ setup.sh
@@ -10,9 +10,21 @@ ln -sfh ${script_dir}/.zshrc ~/.zshrc
 ln -sfh ${script_dir}/.vim ~/.vim
 
 if [ -f ${script_dir}/.zshrc_work ]; then
-  ln -s ${script_dir}/.zshrc_work ~/.zshrc_work
+  ln -sfh ${script_dir}/.zshrc_work ~/.zshrc_work
 fi
 
 if [ "$(uname -s)" = "Darwin" ]; then
   brew bundle
+fi
+
+ln -sfh ${script_dir}/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -sfh ${script_dir}/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+
+if [ -z "$(which code)" ]; then
+  echo "[ERROR] Visual Studio Code is not installed." >&2
+  exit 1
+else
+  for extension in $(cat ${script_dir}/vscode/extensions.txt); do
+    code --install-extension ${extension}
+  done
 fi
